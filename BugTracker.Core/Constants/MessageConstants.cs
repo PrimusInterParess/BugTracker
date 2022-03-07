acda745141320d4583f4b0be49142117569b48cb
Дани Борисов DAniborisov <diesonnekind@gmail.com>
2022-03-07 19:03:19 +0200

    mess

---
+++ BugTracker.Core/Constants/MessageConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace BugTracker.Core.Constatns
+{
+    public static class MessageConstants
+    {
+        public const string ErrorMessage = "ErrorMessage";
+        public const string SuccessMessage = "SuccsessMessage";
+        public const string WarningMessage = "WarningMessage";
+    }
+}
