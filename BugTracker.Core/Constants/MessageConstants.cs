2ad247a28add3438a94c70c99284d90f5e9032e6
Дани Борисов DAniborisov <diesonnekind@gmail.com>
2022-03-16 22:35:03 +0200

    eraser

--- BugTracker.Core/Constants/MessageConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BugTracker.Core.Constatns
-{
-    public static class MessageConstants
-    {
-        public const string ErrorMessage = "ErrorMessage";
-        public const string SuccessMessage = "SuccsessMessage";
-        public const string WarningMessage = "WarningMessage";
-    }
-}
